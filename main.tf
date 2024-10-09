18324179bb0ef66e92a2a5d23dd37e4df235fc08
Jamie Bond <jamie2000bond@gmail.com>
2024-10-09 09:21:30 +0100

    pipelines update

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 provider "azurerm" {
   features {}
+
+    # Add the service principal authentication details here
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
 # Create Resource Group
--- variables.tf
+++ variables.tf
@@ -1,3 +1,30 @@
+# Variables for Service Principal Authentication
+variable "client_id" {
+  description = "The Client ID of the Service Principal"
+  type        = string
+  default     = "d0abc8bf-92b5-4b7a-ae04-6a0625e55d36"
+}
+
+variable "client_secret" {
+  description = "The Client Secret of the Service Principal"
+  type        = string
+  sensitive   = true
+  default     = "eez8Q~wHBUCc0LvFPo2lpFvFZQFPQcuwuTfyaR_"
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID for the Service Principal"
+  type        = string
+  default     = "ca249364-2247-4916-be5f-6878448ff851"
+}
+
+variable "subscription_id" {
+  description = "The Subscription ID where resources will be created"
+  type        = string
+  default     = "a67fa08c-8a71-4843-a6e9-1fbd1d8198b6"
+}
+
+
 variable "resource_group_name" {
   description = "The name of the Azure Resource Group"
   type        = string
