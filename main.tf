96835aea18166071e835b950aed8e2348a8ab08e
Jamie Bond <jamie2000bond@gmail.com>
2024-10-09 16:42:11 +0100

    test

--- main.tf
+++ main.tf
@@ -28,13 +28,14 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_application_insights" "app_insights" {
   name                = var.app_insights_name
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = var.resource_group_name
   application_type    = "web"
   retention_in_days   = 90
 
-  # Remove or comment out workspace_id if already imported and no changes required
-  # workspace_id = var.workspace_resource_id
+  # Set the workspace_id to the actual value in Azure
+  workspace_id        = "/subscriptions/a67fa08c-8a71-4843-a6e9-1fbd1d8198b6/resourceGroups/DefaultResourceGroup-SUK/providers/Microsoft.OperationalInsights/workspaces/DefaultWorkspace-a67fa08c-8a71-4843-a6e9-1fbd1d8198b6-SUK"
 }
+
 
 # Create Storage Account
 resource "azurerm_storage_account" "storage_account" {
