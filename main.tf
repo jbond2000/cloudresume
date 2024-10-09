e6a6d71f18c8582feb976f11d9c9787498c60d48
Jamie Bond <jamie2000bond@gmail.com>
2024-10-09 15:39:26 +0100

    test

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "azurerm_storage_account" "storage_account" {
 
 resource "azurerm_storage_account" "test_storage_account"{
   name = var.test_storage_account_name
-  resource_group_name = azurerm
+  resource_group_name = azurerm_resource_group.rg.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
